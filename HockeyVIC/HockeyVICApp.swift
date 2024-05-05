345287d4f9af47370f61ae2764e2b48d4bc27739
bmoxey <bmoxey@gmail.com>
2024-05-05 13:12:36 +1000

    Initial Commit

---
+++ HockeyVIC/HockeyVICApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyVICApp.swift
+//  HockeyVIC
+//
+//  Created by Brett Moxey on 5/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct HockeyVICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
